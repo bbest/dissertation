92b92b30ef513fe9fe3f540ee54a594c397a9c73
bbest <bbest@nceas.ucsb.edu>
2014-07-29 15:50:42 -0700

    now converting md to tex (overwriting original)

--- duke_template_futz.R
+++ duke_template_futz.R
@@ -4,3 +4,11 @@ for (f_tex in list.files(pattern=glob2rx('*.tex'))){
   cat(f,'\n')
   system(sprintf('pandoc %s.tex -t markdown -o %s.md', f, f))
 }
+
+setwd('~/github/phd_thesis/duke_template')
+for (f_tex in list.files(pattern=glob2rx('*.md'))){
+  f = tools::file_path_sans_ext(f_tex)
+  cat(f,'\n')
+  system(sprintf('pandoc %s.md -t latex -o %s.tex', f, f))
+}
+
