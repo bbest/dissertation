c7fe86d0101eecd6849b3056e073a7c49991a8a0
bbest <bbest@nceas.ucsb.edu>
2014-07-29 15:47:27 -0700

    converted tex to md

---
+++ duke_template_futz.R
@@ -0,0 +1,6 @@
+setwd('~/github/phd_thesis/duke_template')
+for (f_tex in list.files(pattern=glob2rx('*.tex'))){
+  f = tools::file_path_sans_ext(f_tex)
+  cat(f,'\n')
+  system(sprintf('pandoc %s.tex -t markdown -o %s.md', f, f))
+}
