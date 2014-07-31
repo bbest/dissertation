2d45515e34d52324b8042552853e4b54023c414d
bbest <bbest@nceas.ucsb.edu>
2014-07-30 18:17:47 -0700

    working pdf, before cleanup

--- duke_template_futz.R
+++ duke_template_futz.R
@@ -29,6 +29,11 @@ bibtex dissertation
 pdflatex dissertation.tex
 pdflatex dissertation.tex
 
+pandoc --latex-engine=xelatex -H preamble.tex -V fontsize=12pt -V documentclass:book -V papersize:a4paper 
+-V classoption:openright --chapters --bibliography=papers.bib --csl="csl/nature.csl" title.md summary.md 
+zusammenfassung.md acknowledgements.md toc.md "introduction/intro1.md" "introduction/intro2.md" chapter2_paper.md 
+chapter3_extra_results.md chapter4_generaldiscussion.md appendix.md references.md -o "phdthesis.pdf"
+
 
 pandoc --latex-engine=xelatex -H preamble.tex -V fontsize=12pt -V documentclass:book -V papersize:a4paper 
 -V classoption:openright --chapters --bibliography=papers.bib --csl="csl/nature.csl" title.md summary.md
